36417bea939ca38cdaaf7de7e009fc5b577ac765
aji1998 <aji19980419@gmail.com>
2018-11-11 09:59:02 +0700

    delete file dak penting

--- views/includes/top-nav.php
+++
@@ -1,6 +0,0 @@
-<nav id="d-top-nav" class="clearfix">
-    <h3 class="d-title">Dashboard</h3>
-    <div id="toggle-navbar" class="d-right-container">
-        <i class="fa fa-list"></i>
-    </div>
-</nav>
